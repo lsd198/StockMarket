3fbf1ae60e051f76213a290d944b6635b24426b8
lsd198 <dhailal@tcd.ie>
2021-03-05 19:09:50 +0000

    update the code with error/ update the code in main file

--- LoadDatasql.py
+++ LoadDatasql.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import pyodbc
 import datetime
+import main_file
 class loadsqlfile:
 
 
@@ -13,8 +14,9 @@ class loadsqlfile:
                 str(type(comp_data_p)).split('.')[-1].find('DataFrame') == 0:
             orid_data_p['Datetime'] = [datetime.datetime.strptime(i, "%Y-%m-%d %H:%M:%S") for i in orid_data_p.Datetime]
             comp_data_p['Datetime'] = [datetime.datetime.strptime(i, "%Y-%m-%d %H:%M:%S") for i in comp_data_p.Datetime]
-            l_val.extend(i for i in orid_data_p.loc[i])
-            a = self.create_list(orid_data_p, comp_data_p)
+            # have to remove this hardcoded in future to search for any random element
+            ls=[orid_data_p.Datetime.loc[2],comp_data_p.Datetime.loc[2]]
+            a = self.create_list(orid_data_p, comp_data_p) + self.search_dat(ls)
             return a
 
 
@@ -22,12 +24,11 @@ class loadsqlfile:
         l_val = []
         # f_val=[]
         for i in range(len(orid_data_p)):
-            l_val.extend(([i for i in orid_data_p.loc[i]]+[i for i in comp_data_p.loc[i]]))
-            # f_val.append(self.format_modify(l_val.copy()))
-            # l_val.clear()
+            l_val.extend(([i for i in orid_data_p.loc[i]]+[i for i in comp_data_p.loc[i]]+[self.format_modify(l_val.copy())]))
         return l_val
 
     def format_modify(self, list_c):
+        # have to remove the below hardcoded value in future for if condition
         for i in range(len(list_c)):
             if i == 0 or i == 3:
                 list_c[i] = self.dateconversion(list_c[i], i)
@@ -35,18 +36,13 @@ class loadsqlfile:
                 break
         return list_c
 
+    def search_dat(self, ls):
+        val=[]
+
 
 
     def dateconversion(self, list, i):
-        # y = int(list.split(' ')[0].split('-')[0].strip("0"))
-        # m = int(list.split(' ')[0].split('-')[1].strip("0"))
-        # d = int(list.split(' ')[0].split('-')[2].strip("0"))
-        # h = int(list.split(' ')[1].split(':')[0].strip("0"))
-        # if list.split(' ')[1].split(':')[1].strip("0")=='':
-        #     val = datetime.datetime(y, m, d, h)
-        # else:
-        #     val = datetime.datetime(y, m, d, h, int(list.split(' ')[1].split(':')[1].strip("0")))
-        val = datetime.datetime.strptime(list,"%Y-%m-%d %H:%M:%S")
+        val = datetime.datetime.strptime(list, "%Y-%m-%d %H:%M:%S")
         return val
 
     def load_data(self,orid_data, comp_data):
--- main_file.py
+++ main_file.py
@@ -5,19 +5,22 @@ import datetime
 
 
 class start_analysis:
-
-    def load_data(self):
-        start_time = time.perf_counter()
+    def __init__(self):
+        self.start_time = time.perf_counter()
         raw_csv_data = pd.read_csv("StockDtaonminfoyweeklast.csv")
         raw_csv_data = raw_csv_data.set_index('Datetime')
         df_comp = pd.DataFrame(raw_csv_data.Close)
-        df_comp['lag1'] = df_comp.Close-df_comp.Close.shift(1)
+        df_comp['lag1'] = df_comp.Close - df_comp.Close.shift(1)
         df_comp = df_comp.drop(['2021-01-11 09:30:00-05:00'], axis=0)
+        global  self.df_comp = self.date_for_change(df_comp).copy()
+
+
+
+    def load_data(self):
         obj_sm = StockSimilarData.StockSmiliar()
-        df_comp=self.date_for_change(df_comp).copy()
-        obj_sm.train_dat(df_comp)
+        obj_sm.train_dat(self.df_comp)
         finish_time = time.perf_counter()
-        print(f'Finished in {round(finish_time-start_time)/3600} hours(h)')
+        print(f'Finished in {round(finish_time-self.start_time)/3600} hours(h)')
 
     def date_for_change(self,d):
         d.reset_index(inplace=True)
